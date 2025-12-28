30f9d0630f0a67af63197cf1a3685f6ecd2e173d
Br3n0k <br3n0k@gmail.com>
2025-12-28 01:55:36 -0300

    update install script

--- install.sh
+++ install.sh
@@ -1,34 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -euo pipefail
+set -eu
 
-# Instala python e pip
-apk add --no-cache python3 py3-pip
+apk add --no-cache python3 py3-pip git gcc musl-dev libffi-dev openssl-dev openssl python3-dev
 
 # Cria links simbólicos para python3 e pip3
 ln -sf python3 /usr/bin/python
 ln -sf pip3 /usr/bin/pip
 
-# Instala gcc para compilação de extensões
-apk add --no-cache gcc
-
-# Instala musl-dev
-apk add --no-cache musl-dev
-
-# Instala libffi-dev
-apk add --no-cache libffi-dev
-
-# Instala openssl-dev e openssl
-apk add --no-cache openssl-dev openssl
-
-# Instala python3-dev
-apk add --no-cache python3-dev
-
 # Baixa o repositorio
-git clone git@github.com:Nok-Tech/nok-dns.git /opt/nok-dns
+git clone https://github.com/Nok-Tech/nok-dns.git /opt/nok-dns
 
 # Navega para o diretório do repositório
 cd /opt/nok-dns
 
 # Instala as dependências com pyproject.toml
-pip install .
+python3 -m pip install .
